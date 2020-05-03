2c51308248a0abc7989e10c80d8487f48f63f6ba
“Diwakar” <Diwakarsingh052@gmail.com>
2020-05-03 21:46:05 +0530

    Implemented Not FOund

--- main.go
+++ main.go
@@ -18,9 +18,21 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		</h1>`)
 }
 
+
+
+func notfound(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w,"<h1>Oops Page not Found</h1>")
+}
+
 func main() {
 
 	r := mux.NewRouter()
+	// not Found handler needs http.Handler which is implemented by
+	//handlerfunc type  and handlerfunc type implements w http.ResponseWriter, r *http.Request
+	//check docs for more info // video cast 3.2_EX_2
+	r.NotFoundHandler= http.HandlerFunc(notfound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8080", r)
