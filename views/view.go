5d91493429c2c17603bb2c33a7e5f460501c1e81
“Diwakar” <Diwakarsingh052@gmail.com>
2020-05-04 01:32:21 +0530

    Refactored Views.go

--- views/view.go
+++ views/view.go
@@ -1,13 +1,17 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   = "views/layouts/"
+	TemplateExt = ".gohtml"
+)
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-	)
+	files = append(files, layoutFiles()...) //unpacking slice to string
 	t, err := template.ParseFiles(files...) // here ... converts slice to a string
 	if err != nil {
 		panic(err)
@@ -21,4 +25,13 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func layoutFiles() []string {
+
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
