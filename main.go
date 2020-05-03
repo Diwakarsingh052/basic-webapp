bf9148d856e17acf1cb7251f822c1f740e57e81b
“Diwakar” <Diwakarsingh052@gmail.com>
2020-05-04 02:06:26 +0530

    Added Signup Page

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -33,16 +34,24 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Oops Page not Found</h1>")
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	if err := signupView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
 func main() {
 
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 
 	r.NotFoundHandler = http.HandlerFunc(notfound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":8080", r)
 
 }
