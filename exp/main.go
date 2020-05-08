d6cfe6474d53bda4cf4d5dcde7db0fc4bfdea525
“Diwakar” <Diwakarsingh052@gmail.com>
2020-05-08 12:16:35 +0530

    .

--- exp/main.go
+++ exp/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
-	"webdev/rand"
+	"webdev/hash"
 )
 
 func main() {
+	//toHash := []byte("This is the text")
+	//h := hmac.New(sha256.New, []byte("My secret Key"))
+	//h.Write(toHash)
+	//b := h.Sum(nil)
+	//fmt.Println(b)
 
-	fmt.Println(rand.String(10))
-	fmt.Println(rand.RememberToken())
+	hmac := hash.NewHMAC("my-secret-key")
+	fmt.Println(hmac.Hash("This is the text"))
 }
